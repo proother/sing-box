bb2169bc17d50a7eac030dcbf8962453c9747432
世界 <i@sekai.icu>
2026-03-22 06:35:34 +0800

    release: Fix install_go.sh

--- release/local/install_go.sh
+++ release/local/install_go.sh
@@ -2,8 +2,16 @@
 
 set -e -o pipefail
 
-go_version=$(curl -s https://raw.githubusercontent.com/actions/go-versions/main/versions-manifest.json | grep -oE '"version": "[0-9]{1}.[0-9]{1,}(.[0-9]{1,})?"' | head -1 | cut -d':' -f2 | sed 's/ //g; s/"//g')
-curl -Lo go.tar.gz "https://go.dev/dl/go$go_version.linux-amd64.tar.gz"
+manifest=$(curl -fS 'https://go.dev/VERSION?m=text')
+go_version=$(echo "$manifest" | head -1 | sed 's/^go//')
+os=$(uname -s | tr '[:upper:]' '[:lower:]')
+arch=$(uname -m)
+case "$arch" in
+  x86_64) arch="amd64" ;;
+  aarch64|arm64) arch="arm64" ;;
+esac
+curl -Lo go.tar.gz "https://go.dev/dl/go$go_version.$os-$arch.tar.gz"
 sudo rm -rf /usr/local/go
 sudo tar -C /usr/local -xzf go.tar.gz
 rm go.tar.gz
+echo "Installed Go $go_version"
